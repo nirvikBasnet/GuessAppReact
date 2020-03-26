0d96ad4c650ab89f0a8c33e9b98a69c3311cbb32
nirvikBasnet <nirvik41@gmail.com>
2020-03-26 18:23:40 +1100

    first commit

---
+++ Desktop/guessApp/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Nirvik is God</Text>
+//     </View>
+//   );
+// }
+
+export default class Game extends Component {
+  render () {
+    return(
+      <View style={styles.container}> 
+        <Text>Guess my number</Text>
+      </View>
+    )
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ff33ff',
+    fontSize: 32
+  }
+
+});
